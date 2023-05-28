472f05495c6590732b1e326793fd3c85f9744411
Sakal05 <samnang.sakal@gmail.com>
2023-05-28 22:44:51 +0700

    init

--- src/pages/newsFeedCard/index.js
+++ src/pages/newsFeedCard/index.js
@@ -41,9 +41,8 @@ const NewsFeedCard = () => {
               <img
                 src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
                 srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                // src='images\cards\analog-clock.jpg'
-                // alt='testing'
                 loading='lazy'
+                alt='er'
               />
             </ImageListItem>
         ))}
