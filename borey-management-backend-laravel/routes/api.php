adaf7fa8d6c973e5d90a461444969869e1f09d7f
Sokleng <lysokleng3@gmail.com>
2023-06-10 17:43:26 +0700

    Recreate Login Register Reset

--- borey-management-backend-laravel/routes/api.php
+++ borey-management-backend-laravel/routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PasswordResetController;
 
-use App\Http\Controllers\API\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +17,15 @@ use App\Http\Controllers\API\RegisterController;
 |
 */
 
+// Public Routes
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/send-reset-password-email', [PasswordResetController::class, 'send_reset_password_email']);
+Route::post('/reset-password/{token}', [PasswordResetController::class, 'reset']);
 
-
-Route::post('register', [RegisterController::class, 'register']);
-Route::post('login', [RegisterController::class, 'login']);
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected Routes
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/loggeduser', [UserController::class, 'logged_user']);
+    Route::post('/changepassword', [UserController::class, 'change_password']);
 });
